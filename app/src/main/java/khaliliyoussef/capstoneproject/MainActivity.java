47524b24fd81796b4b115bcfcb8da55cbe1c73c6
khaliliyoussef <khalilibrahim865@gmail.com>
2017-08-02 15:18:26 -0700

    add the menu item

--- app/src/main/java/khaliliyoussef/capstoneproject/MainActivity.java
+++ app/src/main/java/khaliliyoussef/capstoneproject/MainActivity.java
@@ -2,6 +2,8 @@ package khaliliyoussef.capstoneproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
